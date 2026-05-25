801e1a3be7b45998f39ffe02adf6d924a4505a4d
FizzleOut <fizzleout1337@gmail.com>
2026-05-25 19:32:23 +0200

    feat(auth): add global auth-based routing
    - Add watcher in app.vue to track useSupabaseUser
    - Implement automatic redirect to / or /login based on auth state
    - Remove navigateTo from signIn in authStore
    - Move navigation responsibility to the app root level

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
-  import { NuxtLayout, NuxtPage } from '#components'
+  import { NuxtLayout, NuxtPage } from '#components';
+  import { useSupabaseUser } from '#imports';
+  import { navigateTo } from 'nuxt/app';
+  import { watch } from 'vue';
+
+  const user = useSupabaseUser();
+
+  watch(user, (newUser) => {
+    if (newUser) {
+      navigateTo('/');
+    } else {
+      navigateTo('/login');
+    }
+  });
 </script>
 
 <template>
